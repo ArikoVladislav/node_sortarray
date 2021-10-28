f2f2664c6001f948adffb81e9bc83cdadc795a84
ArikoVladislav <v.ariko@bk.ru>
2021-10-29 01:26:16 +0300

    creating 2 files txt  and complete validation.

---
+++ module/code.js
@@ -0,0 +1,25 @@
+// module.exports.RemoveDuplicate = function RemoveDuplicate(digits){
+//     let str = '';
+//     for (let i=0;i< digits.length;i++){
+//       for (let j=i+1;j<=digits.length;j++){
+//         const word = digits.slice(i,j)
+//         if(digit.includes(word)){
+//           str += ' ' + word
+//           i=j
+//         }
+//       }
+//     }
+//     return str.trim()
+//   }
+//   const digit = "zero nine one one eight two seven three six four two five".split(' ')
+//   module.exports.RemoveDuplicate = function RemoveDuplicate(arr) {
+//     let result = [];
+  
+//     for (let str of arr) {
+//       if (!result.includes(str)) {
+//         result.push(str);
+//       }
+//     }
+  
+//     return result;
+//   }
